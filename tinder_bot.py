8c9bea1bf583e9b3d2b778650a832c5a9a006d1c
Shubham Prashar <67927586+oyeprashar@users.noreply.github.com>
2021-12-05 19:55:26 +0530

    Update tinder_bot.py

--- tinder_bot.py
+++ tinder_bot.py
@@ -14,6 +14,11 @@ driver = webdriver.Chrome(PATH)
 driver.get("https://tinder.com/?lang=en")
 
 
+# driver.implicitly_wait(7)
+# like_button = WebDriverWait(driver, 100).until(
+#     EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div[2]/div[4]/button'))
+
+#     ) 
 
 
 driver.implicitly_wait(7)
@@ -21,17 +26,48 @@ like_button = WebDriverWait(driver, 100).until(
     EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div[2]/div[4]/button'))
 
     ) 
+
+# dislike_button = WebDriverWait(driver, 100).until(
+# 	EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div[2]/div[2]/button'))
+# 	)
+
 dislike_button = WebDriverWait(driver, 100).until(
 	EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div[2]/div[2]/button'))
 	)
 like_count = 0
 dislike_count = 0
 
+# action_like = ActionChains(driver)
+# action_like.click(like_button)
+
 action_like = ActionChains(driver)
 action_like.click(like_button)
 
 action_dislike = ActionChains(driver)
 action_dislike.click(dislike_button)
+
+# while(True):
+# 	# random time delay to make it look more like a human
+# 	rand_sleep = random.random()
+# 	time.sleep(rand_sleep)
+# 	rand_liker_disliker = random.random()
+	
+# 	#randomly liking and disliking wrt value of random.random() which lie between 0 and 1 
+# 	if rand_liker_disliker < .85:
+# 		action_like.perform()
+
+# 		print("liked =    ",like_count)
+# 		like_count = like_count + 1
+		
+# 		# Giving delay after like_count reaches multiple of 100s
+# 		if like_count % 100 == 0:
+# 			time.sleep(100)
+		
+		
+# 	else:
+# 		action_dislike.perform()
+# 		print("disliked = ",dislike_count)
+# 		dislike_count = dislike_count + 1
 
 while(True):
 	# random time delay to make it look more like a human
